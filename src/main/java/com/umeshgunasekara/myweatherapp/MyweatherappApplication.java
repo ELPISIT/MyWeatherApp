5b91be5e90487b0e29c80721dd87baea766ffa9d
Umesh Gunasekara <umesh.gunasekara.n@gmail.com>
2019-10-04 22:50:48 +0800

    First commit of MyWeatherApp with initial SpringBoot Project

---
+++ src/main/java/com/umeshgunasekara/myweatherapp/MyweatherappApplication.java
@@ -0,0 +1,13 @@
+package com.umeshgunasekara.myweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyweatherappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyweatherappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umeshgunasekara/myweatherapp/MyweatherappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.umeshgunasekara.myweatherapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyweatherappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
